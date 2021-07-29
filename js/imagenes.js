b054ad19b531c986ca58b573e42444af7dd3b4c6
ewewx <77701500+ewewx@users.noreply.github.com>
2021-07-29 12:16:28 -0300

    Update imagenes.js

--- js/imagenes.js
+++ js/imagenes.js
@@ -1,37 +1,47 @@
+
 function iniciar(){
    var imagenes1= document.querySelectorAll(".imagenes1")
    var imagenesCant=imagenes1.length;
    for (i=0; i<imagenesCant; i++){
-     
+     imagenes1[i].addEventListener('drag',function seleccion(e){
+        
+     })
       imagenes1[i].addEventListener("dragstart",function arrastrado (e){
          elemento=e.target;
-         e.dataTransfer.setData('Text', elemento.getAttribute('id'));
-         e.dataTransfer.getData('Text')
-         console.log(elemento) 
-         },false)
+        elementoDiv=elemento.parentNode.parentNode
+         e.dataTransfer.setData('Text', elemento.getAttribute('src'));
          
-         imagenes1[i].addEventListener("dragenter",entrando,false)
+         console.log(elementoDiv)
+          
+         },false)
+   
+         imagenes1[i].addEventListener("dragenter", function entrando (e){
+     e.preventDefault();
+     vari=e.target
+     variable=vari.cloneNode(true)
+     e.dataTransfer.setData('Text/HTML',variable.getAttribute('src'))
+    
+  },false)
          imagenes1[i].addEventListener("dragleave",saliendo,false)
          imagenes1[i].addEventListener("dragover",function (e){
          e.preventDefault() }, false)
-        
+         
          imagenes1[i].addEventListener("drop", function soltado(e){
             e.preventDefault();
-            var slot=e.target;
-            e.dataTransfer.getData('Text');
-            e.dataTransfer.setData("Text",slot.getAttribute('id'))
+            slot=e.target;
+            e.dataTransfer.getData('Text/HTML');
+            e.dataTransfer.setData('Text/HTML',slot.getAttribute('src'))
             slot.src=elemento.getAttribute("src")
-            
-      console.log(slot)
+            elemento.src=variable.getAttribute('src')
+            console.log(variable)
       },false)
-   }  
+     
+     }  
       }
-  function entrando (e){
-     e.preventDefault();
-    
-  }
+ 
   function saliendo (e){
      e.preventDefault();
+     
   }
  
 
